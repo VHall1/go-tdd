9e6594c8c7552fb45fe3dd6cd510ae188ee1fb88
vhall1 <victorhallpsn@hotmail.com>
2024-11-18 16:58:10 +0000

    write failing tests for Wallet

---
+++ banking/wallet.go
@@ -0,0 +1,11 @@
+package banking
+
+type Wallet struct{}
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return 0
+}
---
+++ banking/wallet_test.go
@@ -0,0 +1,15 @@
+package banking
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	w := Wallet{}
+	w.Deposit(10)
+
+	got := w.Balance()
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
