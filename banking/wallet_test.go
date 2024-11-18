e2605280199ecfbb223b59059f770f3c5070ded4
vhall1 <victorhallpsn@hotmail.com>
2024-11-18 17:10:09 +0000

    refactor: clean up tests

--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -3,27 +3,24 @@ package banking
 import "testing"
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		w := Wallet{}
-		w.Deposit(Bitcoin(10))
-
-		got := w.Balance()
-		want := Bitcoin(10)
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		w := Wallet{}
+		w.Deposit(Bitcoin(10))
+		assertBalance(t, w, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		w := Wallet{balance: Bitcoin(20)}
 		w.Withdraw(Bitcoin(10))
-
-		got := w.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, w, Bitcoin(10))
 	})
 }
