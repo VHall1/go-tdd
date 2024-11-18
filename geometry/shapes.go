813e7a98a3a4eb205c13e4b60b77703641b08de6
VHall1 <victorhallpsn@hotmail.com>
2024-11-18 01:08:53 +0000

    refactor: implement Rectangle struct and convert funcs into methods

--- geometry/shapes.go
+++ geometry/shapes.go
@@ -1,9 +1,13 @@
 package main
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width, Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
