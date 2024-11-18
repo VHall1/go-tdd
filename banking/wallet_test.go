7675cd1edf1d2178507aacb437d124b49a4b9686
vhall1 <victorhallpsn@hotmail.com>
2024-11-18 17:08:11 +0000

    add failing test to withdraw from Wallet

--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -3,13 +3,27 @@ package banking
 import "testing"
 
 func TestWallet(t *testing.T) {
-	w := Wallet{}
-	w.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		w := Wallet{}
+		w.Deposit(Bitcoin(10))
 
-	got := w.Balance()
-	want := Bitcoin(10)
+		got := w.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		w := Wallet{balance: Bitcoin(20)}
+		w.Withdraw(Bitcoin(10))
+
+		got := w.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
