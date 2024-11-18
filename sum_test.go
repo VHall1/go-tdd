2514a6d8f5997374d81a8e03344efe6e6a9d431c
vhall1 <victorhallpsn@hotmail.com>
2024-11-18 00:18:24 +0000

    add failing test for SumAll

--- sum_test.go
+++ sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"testing"
 )
 
@@ -12,5 +13,14 @@ func TestSum(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
