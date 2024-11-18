a1d9b7da3517ce8a29d75476734b2a36892eb456
vhall1 <victorhallpsn@hotmail.com>
2024-11-18 17:04:35 +0000

    refactor: implement Bitcoint type

--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	w := Wallet{}
-	w.Deposit(10)
+	w.Deposit(Bitcoin(10))
 
 	got := w.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
